9dbd47467dca8e7df0c2b9365541723bc1788ab0
Jordan <jbrockerrudow@gmail.com>
2022-07-06 11:34:40 -0500

    Finish asset structure & app routing initial setup

--- rngloot/src/app/app.module.ts
+++ rngloot/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
+    //NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
