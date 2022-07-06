d74b8aa5c1542b3b4ca2284ce15e2abd2f14f07f
Jordan Brocker Rudow <Jordan.Rudow@gmail.com>
2022-07-06 11:36:17 -0500

    V1.0.0/angular (#1)
    * Start Angular Application
    
    * Config - Add Bootstrap & dependencies
    
    * Setup basic essential styles and dir structure
    
    * Initialize layout component - footer
    
    * Initialize layout component - header
    
    * Initialize page component - home
    
    * Create service for custom item input
    
    * create service for fetching equpiment data (external)
    
    * Finish asset structure & app routing initial setup

---
+++ rngloot/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    //NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
