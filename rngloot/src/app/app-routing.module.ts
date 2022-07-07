2f75bcf9327f3d11e5afe59dac812609621e291b
Jordan <jbrockerrudow@gmail.com>
2022-07-07 09:19:26 -0500

    Update imports for new structure & wildcard routing

--- rngloot/src/app/app-routing.module.ts
+++ rngloot/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotfoundpageComponent } from './pages/notfoundpage/notfoundpage.component';
+
 
 const routes: Routes = [
   {
@@ -11,6 +13,10 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     redirectTo: 'home'
+  },
+  {
+    path: '**',
+    component: NotfoundpageComponent
   }
 ];
 
--- rngloot/src/app/app.module.ts
+++ rngloot/src/app/app.module.ts
@@ -6,14 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 //import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotfoundpageComponent } from './pages/notfoundpage/notfoundpage.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { LootdetailsComponent } from './pages/lootdetails/lootdetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    NotfoundpageComponent,
+    MenuComponent,
+    LootdetailsComponent
   ],
   imports: [
     BrowserModule,
