b40c860b604654b7ac95f3ecccc6b9a318db3293
Jordan <jbrockerrudow@gmail.com>
2022-07-08 10:42:01 -0500

    Create temp models to reflect api data

--- rngloot/src/app/models/equipmentItem.ts
+++ rngloot/src/app/models/equipmentItem.ts
@@ -1,3 +1,29 @@
-export class EquipmentItem {
+export class EquipmentCategory {
+    index: string;
     name: string;
-}
+    url: string;
+};
+export class ToolCategory {
+    index: string;
+    name: string;
+    url: string;
+};
+export class Cost {
+    quantity: number;
+    unit: string;
+};
+
+
+export class EquipmentItem {
+    special: any;
+    index: string;
+    name: string;
+    equipment_category: [EquipmentCategory];
+    tool_category: string;
+    cost: [Cost];
+    weight: 6,
+    desc: any;
+    url: string;
+    contents: any;
+    properties: any;
+};
