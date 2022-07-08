7bbdbe5acf86b379ef03b8e1dfafe19625a90935
Jordan <jbrockerrudow@gmail.com>
2022-07-08 10:42:35 -0500

    Add Detail services for equpiment items

--- rngloot/src/app/services/equipment.service.ts
+++ rngloot/src/app/services/equipment.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Equipment } from '../models/equipment';
+import { EquipmentItem } from '../models/equipmentItem';
 import { INDEXITEMS } from '../shared/equipmentIndex';
+import { EQUIPMENTITEMS } from '../shared/equipmentItems';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,17 @@ export class EquipmentService {
   getIndexItems(): Equipment[] {
     return INDEXITEMS;
   }
+
+  getIndexItem(name: string): Equipment {
+    return INDEXITEMS.filter((indexitem) => (indexitem.name === name))[0];
+  }
+
+  getEquipmentItems(): EquipmentItem[] {
+    return EQUIPMENTITEMS;
+  }
+
+  getEquipmentItem(index: string): EquipmentItem {
+    return EQUIPMENTITEMS.filter((equpimentitem) => (equpimentitem.index === index))[0];
+  }
+  
 }
